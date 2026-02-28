6eaf023f17b0d19e9df9e766d0bf4f14bac70a8f
jonghyun <jhs.on.lab@gmail.com>
2026-02-28 16:35:54 +0900

    chore: remove unused code

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,14 +4,6 @@ export const signup = (payload) => {
   return apiClient.post('users', payload)
 }
 
-export const getUsers = (payload) => {
-  return apiClient.get('users', { params: payload })
-}
-
 export const getMyInfo = () => {
   return apiClient.get('users/me')
-}
-
-export const getSocialLinkUrl = (provider) => {
-  return apiClient.get('auth/oauth2/link/' + provider)
 }
